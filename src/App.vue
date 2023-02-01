7bb7da53ce7f141fe0c0b63cbf615c852bc7f2be
cozercc <cozercn@gmail.com>
2023-02-01 15:41:06 +0900

    demo:button

--- src/App.vue
+++ src/App.vue
@@ -1,124 +1,147 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import { reactive, ref } from "vue";
+import { reactive, ref, onMounted } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import Demo from "./components/Demo.vue";
+
+const foo = reactive({ count: 0 });
 
 const test = ref(true);
 
 function increment() {
-  test.value = !test.value;
+    test.value = !test.value;
 }
 
 const myObject = reactive({
-  title: "How to do lists in Vue",
-  author: "Jane Doe",
-  publishedAt: "2016-04-10",
+    title: "How to do lists in Vue",
+    author: "Jane Doe",
+    publishedAt: "2016-04-10",
 });
 
 const name = ref("Vue.js");
 
-function greet(event) {
-  alert(`hello ${name.value}`);
-  if (event) {
-    alert(event.target.tagName);
-  }
+const checked = ref(true);
+
+function greet(message) {
+    alert(`hello ${message}`);
 }
+
+onMounted(() => {
+    console.log("123213");
+});
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
+    <header>
+        <img
+            alt="Vue logo"
+            class="logo"
+            src="@/assets/logo.svg"
+            width="125"
+            height="125"
+        />
+
+        <div class="wrapper">
+            <HelloWorld msg="You did it!" />
+
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+            <div>
+                <button @click="increment">{{ test }}</button>
+                <button @click.once="greet('111')">Greet</button>
+            </div>
+            <p v-if="test">YES</p>
+            <p v-else>NO</p>
+            <ul>
+                <li v-for="(value, key, index) in myObject">
+                    {{ key }} : {{ value }} : {{ index }}
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- <RouterView /> -->
+    <p>Message is: {{ foo.count }}</p>
+    <input v-model="foo.count" />
+
+    <p>Message is: {{ foo.count }}</p>
+    <input v-model="foo.count" />
+
+    <input
+        type="checkbox"
+        id="checkbox"
+        v-model="checked"
+        true-value="yes"
+        false-value="no"
     />
+    <label for="checkbox">{{ checked }}</label>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-      <div>
-        <button @click="increment">{{ test }}</button>
-        <button @click="greet">Greet</button>
-      </div>
-      <p v-if="test">YES</p>
-      <p v-else>NO</p>
-      <ul>
-        <li v-for="(value, key, index) in myObject">
-          {{ key }} : {{ value }} : {{ index }}
-        </li>
-      </ul>
-    </div>
-  </header>
-
-  <RouterView />
+    <!-- 示例 -->
+    <Demo />
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+    line-height: 1.5;
+    max-height: 100vh;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    display: block;
+    margin: 0 auto 2rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+    color: var(--color-text);
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+    background-color: transparent;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
 }
 
 nav a:first-of-type {
-  border: 0;
+    border: 0;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+    nav {
+        text-align: left;
+        margin-left: -1rem;
+        font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+        padding: 1rem 0;
+        margin-top: 1rem;
+    }
 }
 </style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+
+const message = ref("Hello World");
+
+function reverseMessage() {
+    message.value = message.value.split("").reverse().join("");
+}
+
+function notify() {
+    alert("WTF");
+}
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+
+    <button @click="reverseMessage">Reverse Message</button>
+
+    <button v-on:click="notify">alert</button>
+
+    <button @click="message += 666">@click="message += 666"</button>
+
+    <button @click="message = 'First'">Clear All</button>
+
+    <a href="https://vuejs.org" @click.prevent="notify">
+        A link with e.preventDefault()
+    </a>
+</template>
+<style>
+
+</style>
