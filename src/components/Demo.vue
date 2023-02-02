b16d563d823a5ced71b215362fd7de93b4f57702
cozercc <cozercn@gmail.com>
2023-02-02 18:03:05 +0900

    コンポーネントのv-model

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,21 +1,11 @@
 <script setup>
 import { ref } from "vue";
-import TodoItem from "./TodoItem.vue";
+import CustomInput from "./CustomInput.vue";
 
-const groceryList = ref([
-    { id: 0, text: "Vegetables" },
-    { id: 1, text: "Cheese" },
-    { id: 2, text: "Meat" },
-]);
+const message = ref("hello");
 </script>
 
 <template>
-    <ol>
-        <TodoItem
-            v-for="item in groceryList"
-            :todo="item"
-            :key="item.id"
-        ></TodoItem>
-    </ol>
+    <CustomInput v-model="message" /> {{ message }}
 </template>
 <style></style>
