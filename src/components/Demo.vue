e633183bcc712b2206b3ee5f677bc91044a38ce1
cozercc <cozercn@gmail.com>
2023-02-02 09:55:40 +0900

    コンポーネント

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,55 +1,21 @@
 <script setup>
 import { ref } from "vue";
+import TodoItem from "./TodoItem.vue";
 
-const text = ref("Edit me");
-const checked = ref(true);
-const checkedNames = ref(["Jack"]);
-const picked = ref("One");
-const selected = ref("A");
-const multiSelected = ref(["A"]);
+const groceryList = ref([
+    { id: 0, text: "Vegetables" },
+    { id: 1, text: "Cheese" },
+    { id: 2, text: "Meat" },
+]);
 </script>
 
 <template>
-    <h2>Text Input</h2>
-    <input v-model="text" /> {{ text }}
-
-    <h2>CheckBox</h2>
-    <input type="checkbox" id="checkbox" v-model="checked" />
-    <label for="checkbox">Checked: {{ checked }}</label>
-
-    <h2>Multi CheckBox</h2>
-    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
-    <label for="jack">Jack</label>
-    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
-    <label for="john">John</label>
-    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
-    <label for="mike">Mike</label>
-    <p>Checked names: {{ checkedNames }}</p>
-
-    <h2>Radio</h2>
-    <input type="radio" id="one" value="One" v-model="picked" />
-    <label for="one">One</label>
-    <br />
-    <input type="radio" id="two" value="Two" v-model="picked" />
-    <label for="two">Two</label>
-    <br />
-    <span>Picked: {{ picked }}</span>
-
-    <h2>Select</h2>
-    <select v-model="selected">
-        <option disabled value="">Please select one</option>
-        <option value="1">A</option>
-        <option value="2">B</option>
-        <option value="3">C</option>
-    </select>
-    <span>Selected: {{ selected }}</span>
-
-    <h2>Multi Select</h2>
-    <select v-model="multiSelected" multiple style="width: 100px">
-        <option>A</option>
-        <option>B</option>
-        <option>C</option>
-    </select>
-    <span>Selected: {{ multiSelected }}</span>
+    <ol>
+        <TodoItem
+            v-for="item in groceryList"
+            :todo="item"
+            :key="item.id"
+        ></TodoItem>
+    </ol>
 </template>
 <style></style>
